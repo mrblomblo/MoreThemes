4b7d1285bd9a1b9cc7e4697fcaa8c3147aa0bf0c
Blomblo <63555404+mrblomblo@users.noreply.github.com>
2025-09-23 19:05:50 +0200

    Change order of Rose Pine themes & correct extention name in logs

--- MoreThemes.cs
+++ MoreThemes.cs
@@ -12,8 +12,8 @@ public class MoreThemes : Extension
     {
         OtherAssets.Add("Assets/nord_polar_night.css");
         OtherAssets.Add("Assets/nord_snow_storm.css");
-        OtherAssets.Add("Assets/rose_pine_moon.css");
         OtherAssets.Add("Assets/rose_pine.css");
+        OtherAssets.Add("Assets/rose_pine_moon.css");
         OtherAssets.Add("Assets/rose_pine_dawn.css");
         OtherAssets.Add("Assets/gruvbox_dark_hard.css");
         OtherAssets.Add("Assets/gruvbox_dark.css");
@@ -26,12 +26,12 @@ public class MoreThemes : Extension
 
     public override void OnInit()
     {
-        Logs.Init("More Themes Extension initializing...");
+        Logs.Init("MoreThemes Extension initializing...");
 
         Program.Web.RegisterTheme(new("nord_polar_night", "Nord Polar Night (MoreThemes)", ["css/themes/modern.css", "/ExtensionFile/MoreThemes/Assets/nord_polar_night.css"], true));
         Program.Web.RegisterTheme(new("nord_snow_storm", "Nord Snow Storm (MoreThemes)", ["css/themes/modern.css", "/ExtensionFile/MoreThemes/Assets/nord_snow_storm.css"], false));
-        Program.Web.RegisterTheme(new("rose_pine_moon", "Rosé Pine Moon (MoreThemes)", ["css/themes/modern.css", "/ExtensionFile/MoreThemes/Assets/rose_pine_moon.css"], true));
         Program.Web.RegisterTheme(new("rose_pine", "Rosé Pine (MoreThemes)", ["css/themes/modern.css", "/ExtensionFile/MoreThemes/Assets/rose_pine.css"], true));
+        Program.Web.RegisterTheme(new("rose_pine_moon", "Rosé Pine Moon (MoreThemes)", ["css/themes/modern.css", "/ExtensionFile/MoreThemes/Assets/rose_pine_moon.css"], true));
         Program.Web.RegisterTheme(new("rose_pine_dawn", "Rosé Pine Dawn (MoreThemes)", ["css/themes/modern.css", "/ExtensionFile/MoreThemes/Assets/rose_pine_dawn.css"], false));
         Program.Web.RegisterTheme(new("gruvbox_dark_hard", "gruvbox dark hard (MoreThemes)", ["css/themes/modern.css", "/ExtensionFile/MoreThemes/Assets/gruvbox_dark_hard.css"], true));
         Program.Web.RegisterTheme(new("gruvbox_dark", "gruvbox dark (MoreThemes)", ["css/themes/modern.css", "/ExtensionFile/MoreThemes/Assets/gruvbox_dark.css"], true));
@@ -41,6 +41,6 @@ public class MoreThemes : Extension
         Program.Web.RegisterTheme(new("gruvbox_light_soft", "gruvbox light soft (MoreThemes)", ["css/themes/modern.css", "/ExtensionFile/MoreThemes/Assets/gruvbox_light_soft.css"], false));
         Program.Web.RegisterTheme(new("solarized_dark", "Solarized Dark (MoreThemes)", ["css/themes/modern.css", "/ExtensionFile/MoreThemes/Assets/solarized_dark.css"], true));
 
-        Logs.Init("More Themes Extension initialized successfully");
+        Logs.Init("MoreThemes Extension initialized successfully");
     }
 }
